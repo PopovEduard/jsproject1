bc7183b2667708f5fc6dfb53f26d80b4daf32bce
Eduard <popoveduard@yandex.ru>
2020-01-26 02:03:11 +0300

    перовое задание

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 'use script';
 
-var i=1;
+let bud = prompt("Ваш бюджет на месяц?", 1);
 
-console.log(i);
+console.log(bud);
+let timeData=prompt("Введите дату в формате YYYY-MM-DD");
+let appdata={
+    bud: bud,
+    timeData: timeData,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+let first=prompt("Введите обязательную статью расходов в этом месяце");
+appdata.optionalExpenses.first = prompt("“Во сколько обойдется?”");
+alert(bud/30);
+
+
+
+
